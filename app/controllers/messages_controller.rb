0e4e4a1d23a538ee4cc03d67874c1e6919c92047
slague <stephanielague@gmail.com>
2017-11-21 09:09:39 -0700

    Update controller to include text

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,23 +5,23 @@ class MessagesController < ApplicationController
   end
 
   def new
-   @message = Message.new
+    @message = Message.new
   end
 
   def create
     @message = Message.new(message_params)
-   if @message.save
+    if @message.save
       flash[:success] = "Your message has been sent! It will appear on John's MagicMirror soon."
-     redirect_to new_message_path
-   else
-     flash.now[:danger] = "#{@message.errors.messages.first[0]} #{@message.errors.messages.first[1][0]}"
-     render :new
-   end
+      redirect_to new_message_path
+    else
+      flash.now[:danger] = "Try again."
+      render :new
+    end
   end
 
   private
   def message_params
-    params.permit(:message, :sender)
+    params.require(:message).permit(:text, :sender)
   end
 
 end
